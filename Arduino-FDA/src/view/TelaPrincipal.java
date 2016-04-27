6310a42c9b268ec8506a837c1d6b72927788db1a
JUNIOR GUEDES <junior.uast@gmail.com>
2016-04-26 21:18:12 -0300

    Delay da inserção

--- Arduino-FDA/src/view/TelaPrincipal.java
+++ Arduino-FDA/src/view/TelaPrincipal.java
@@ -10,6 +10,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import model.LeituraSensores;
 import model.RelatorioDiario;
@@ -20,91 +21,106 @@ public class TelaPrincipal extends JFrame {
 
 	private ControlePersistencia controlePersistencia;
 	private JLabel lbTemperatura, lbUmidade, lbLuminosidade;
-	private JLabel lbValorTemp, lbValorUmid, lbValorLum;
-	private JPanel painel1, painel2;
-	private JButton jbIniciarSensoriamento, jbConfigurarInsercao, jbInserirMortalidade;
+	private JLabel lbValorTemp, lbValorUmid, lbValorLum, lbDelay;
+	private JTextField tfDelay;
+	private JPanel painel1;
+	private JButton jbIniciarSensoriamento, jbModificarDalay, jbInserirMortalidade;
 
 	public TelaPrincipal() {
 
 		super("MONITORAMENTO DA GRANJA");
 		setLayout(new FlowLayout());
-		setVisible(true);
 		setLocationRelativeTo(null);
-		setSize(500, 400);
+		setSize(450, 400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		painel1 = new JPanel(new GridLayout(2, 3, 50, 0));
-		painel2 = new JPanel(new FlowLayout(0, 10, 30));
 		add(painel1);
-		add(painel2);
-		
+
 		Font fonteGrande = new Font("arial", Font.BOLD, 35);
 
 		lbTemperatura = new JLabel("Temperatura:");
 		lbValorTemp = new JLabel("00");
 		lbValorTemp.setFont(fonteGrande);
-		
+
 		lbUmidade = new JLabel("Umidade:");
 		lbValorUmid = new JLabel("00");
 		lbValorUmid.setFont(fonteGrande);
-		
+
 		lbLuminosidade = new JLabel("Luminosidade:");
 		lbValorLum = new JLabel("00");
 		lbValorLum.setFont(fonteGrande);
-		
+
 		painel1.add(lbTemperatura);
 		painel1.add(lbUmidade);
 		painel1.add(lbLuminosidade);
 		painel1.add(lbValorTemp);
 		painel1.add(lbValorUmid);
 		painel1.add(lbValorLum);
-		
+
 		jbIniciarSensoriamento = new JButton("Iniciar Sensoriamento");
-		jbConfigurarInsercao = new JButton("Configurar Inserão");
 		jbInserirMortalidade = new JButton("Inserir Mortalidade");
-		
-		painel2.add(jbIniciarSensoriamento);
-		painel2.add(jbInserirMortalidade);
-		painel2.add(jbConfigurarInsercao);
-		
+
+		lbDelay = new JLabel("Digite o delay (em min): ");
+		tfDelay = new JTextField(5);
+		jbModificarDalay = new JButton("Definir Delay");
+
+		add(jbIniciarSensoriamento);
+		add(jbInserirMortalidade);
+		add(lbDelay);
+		add(tfDelay);
+		add(jbModificarDalay);
+
+
 		jbIniciarSensoriamento.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-			
+
 				controlePersistencia = new ControlePersistencia(TelaPrincipal.this);
-				
-				
+
+
 			}
 		});
-		
+
 		jbInserirMortalidade.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+
 				//Inserir um relatorio da BD apenas p/ testar as inserções da LeituraSensores
 				RelatorioDiario relatorio = new RelatorioDiario();
 				RelatorioDiarioDAO relDAO = new RelatorioDiarioDAO();
 				relatorio.setData("10/10");
 				relatorio.setMortalidade(12);
 				relDAO.inserirRelatorio(relatorio);
-				
+
 			}
 		});
-		
+
+		jbModificarDalay.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				int delayEmSegundos = Integer.parseInt(tfDelay.getText());
+				delayEmSegundos = delayEmSegundos*60;
+				controlePersistencia.setDelay(delayEmSegundos);
+			}
+		});
+
 	}
-	
+
 	public void atualizarResultados(LeituraSensores leitura) {
-		
+
 		lbValorTemp.setText("" + leitura.getTemperatura());
 		lbValorUmid.setText("" + leitura.getUmidade());
 		lbValorLum.setText("" + leitura.getLuminosidade());
 	}
-	
+
 
 	public static void main(String[] args) {
-		new TelaPrincipal();
+		new TelaPrincipal().setVisible(true);
 	}
 
 }
